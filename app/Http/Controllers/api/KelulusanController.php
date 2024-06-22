2d90605bfad885958f3517e7d3a5410fcd37c4f3
layn25 <ryandarmayasaok@gmail.com>
2024-06-23 01:09:29 +0800

    hapus perbaiki

--- app/Http/Controllers/api/KelulusanController.php
+++ app/Http/Controllers/api/KelulusanController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bullying;
 use App\Models\Kelulusan;
+use App\Models\Pelanggaran;
+use App\Models\Prestasi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -60,9 +63,21 @@ class KelulusanController extends Controller
     public function destroy($id)
     {
         $siswa = Kelulusan::find($id);
+        
         if (!$siswa) {
             return response()->json(['message' => 'Kelulusan tidak ditemukan'], 404);
         }
+        $prestasi = Prestasi::where('siswaId', $id);
+        $kelulusan = Kelulusan::where('siswaId', $id);
+        $pelangaran = Pelanggaran::where('siswaId', $id);
+        $bullying = Bullying::where('pelaku', $id);
+        $bullying2 = Bullying::where('korban', $id);
+        $prestasi->delete();
+        $kelulusan->delete();
+        $pelangaran->delete();
+        $bullying->delete();
+        $bullying2->delete();
+        
         $siswa->delete();
         return response()->json(['message' => 'Kelulusan berhasil dihapus'], 200);
     }
